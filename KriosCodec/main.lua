2203bffc696ce97455121318a99b8dc0f243de76
The-Poire <naelbony@free.fr>
2023-08-29 10:43:42 +0200

    Almost Fixed comment filterer

--- KriosCodec/main.lua
+++ KriosCodec/main.lua
@@ -25,6 +25,20 @@ function table.merge(t1,t2,mode)
     end
 end
 
+local function comp(a,b)
+    --[[if type(a) == "table" then
+        if type(b) == "table" then return a[1]<b[2] end
+        return false
+    end
+    if type(b) == "table" then
+        if type(a) == "table" then return a[1]<b[2] end
+        return false
+    end]]
+    if type(a) == "table" then a = a[1] end
+    if type(b) == "table" then b = b[1] end
+    return a<b
+end
+
 local function parser(txt)
 
     local initial_file_size = #txt
@@ -33,46 +47,101 @@ local function parser(txt)
     local single_line_comments,multiline_comments = {},{}
 
         --single line comments
-        for l in string.gmatch(txt, "\\[|]+()") do
-            print(l)
-            single_line_comments[#single_line_comments+1] = l
+        for v in string.gmatch(txt, "\\[|]+()") do
+            print(v,string.sub(txt,v,v))
+            single_line_comments[#single_line_comments+1] = v
         end
 
         --multi-line comments
-        for l in string.gmatch(txt, "\\[*]+()") do
+        for v in string.gmatch(txt, "\\[*]+()") do
             --print(-l)
             if type(multiline_comments[#multiline_comments]) ~= "table" and multiline_comments[#multiline_comments] ~= nil and multiline_comments[#multiline_comments] < 0 then
-                multiline_comments[#multiline_comments] = {-multiline_comments[#multiline_comments],l}
+                multiline_comments[#multiline_comments] = {-multiline_comments[#multiline_comments],v}
             else
-                multiline_comments[#multiline_comments+1] = -l
+                multiline_comments[#multiline_comments+1] = -v
             end
         end
         --print(#comments,comments[#comments][1],comments[#comments][2])
-    table.merge(single_line_comments,multiline_comments)
+
+        --sorting table
+        table.merge(single_line_comments,multiline_comments)
+        --single_line_comments = multiline_comments
+        table.sort(single_line_comments,comp)
+        --table.sort(multiline_comments,comp)
+        --multiline_comments = nil
+        print("---------------------")
+        table.print(single_line_comments)
+        print("---------------------")
+        --table.print(multiline_comments)
+        --print("---------------------")
 
     -- setting up comment clean file
-    local lastChar,last_lenght = 0,0
-    for i,v in ipairs(single_line_comments) do
-        last_lenght = #txt
+    local file,buffer,maxChar = "",0,0
+    for i, v in ipairs(single_line_comments) do
         if type(v) == "number" then
-            txt = string.sub(txt,string.find(txt,"\n",v) + lastChar,-1)
-            lastChar = last_lenght - #txt
+            file = file.."\n"..string.sub(txt,buffer , v - 3)--string.find(txt,"\n",v - 2) + 1)
+            buffer = (string.find(txt,"\n",v - 2) or #txt - 2) + 2
+
+            if maxChar < v then maxChar = v end
+            --print("N",v,string.sub(txt,v,v),#txt)
+        elseif type(v) == "table" then
+            --file = file..string.sub(txt,v[2],#txt) --(string.sub(txt,0,v[1]-3) .. string.sub(txt,v[2],-1))
+            --file = file..string.sub(txt,buffer,#txt)
+
+            if maxChar < v[2] then maxChar = v[2] end
+            buffer = v[2]
+            --print("T",maxChar,string.sub(txt,maxChar,maxChar),#txt)
+        end
+        --print(file,"\n-----",i,v,string.sub(txt,v,v),"-----")
+    end
+    file = file.."\n"..string.sub(txt,buffer - 1,#txt)
+    --print(file,"\n-----")
+    txt = file
+    --print("\n-------------------\n\n",txt)
+
+    --[[local lastChar,last_lenght = 0,0
+    for i,v in ipairs(single_line_comments) do
+        if type(v) == "number" then
+            --print(single_line_comments[i] - 2)
+            print(v - 3,string.find(txt,"\n",v - 2) + 1,"\n-------")
+            --txt = string.sub(txt,string.find(txt,"\n",v + 2) -i + 1,-1)-- + lastChar,-1)
+            txt = string.sub(txt,0,v - 3)..string.sub(txt,string.find(txt,"\n",v - 2) + 1 - lastChar)
         elseif type(v) == "table" then
             lastChar = initial_file_size - #txt
             --print(v[1],v[2],string.sub(txt,0,v[1]-2) .. string.sub(txt,v[2]+2,-1))
             txt = string.sub(txt,0,v[1]-3 - lastChar) .. string.sub(txt,v[2] - lastChar,-1)
         end
         print(txt,"\n-----------\n\n")
-    end
+    end]]
 
     print(txt,"\n\n-------\n")
 
     --Spliting keywords
     local keywords = {}
-    for k in txt:gmatch("%S+") do
+    local _i = {}
+    for k in txt:gmatch("[^\n]+") do
+        keywords[#keywords+1] = k.." ;"
+    end
+
+    _i = keywords
+    _i = table.concat(_i,"\n")
+    keywords = {}
+    for k in _i:gmatch("%S+") do
         keywords[#keywords+1] = k
     end
 
+    _i = 1
+    for k in txt:gmatch("%\n+()") do
+        --keywords[_i] = k.." "..keywords[_i]
+        print(txt)
+        if string.sub(txt,k,k) == "\n" then
+            table.insert(keywords,_i,";")
+            --print(keywords[_i],_i,string.sub(txt,k,k),k)
+        end
+        _i = _i + 2
+    end
+
+    local assembly = {}
     for k,v in ipairs(keywords) do
         
     end
